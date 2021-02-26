a543cd3f3587b291d235825945e633e9a7b2f95f
havila.oliveira <h.oliveira556@gmail.com>
2021-02-26 10:03:29 -0300

    Ajustado.

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -6,6 +6,7 @@ use App\Providers\RouteServiceProvider;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class RedirectIfAuthenticated
 {
@@ -19,6 +20,15 @@ class RedirectIfAuthenticated
      */
     public function handle(Request $request, Closure $next, ...$guards)
     {
+        $data_hora = date('d/m/Y H:i:s');
+        $produtoEditando = DB::table('users')
+            ->where('email', $request->email)
+            ->update(
+            [
+                'ultimo_login' => $data_hora
+            ]
+        );
+
         $guards = empty($guards) ? [null] : $guards;
 
         foreach ($guards as $guard) {
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,14 +13,22 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
+        $dataAtual = date('d/m/Y H:i:s');
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('senha_vista');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->integer('almoxarifado')->default(0);
+            $table->integer('financeiro')->default(0);
+            $table->integer('logistica')->default(0);
+            $table->integer('pcp')->default(0);
+            $table->string('ultimo_login', 255);
+
         });
     }
 
